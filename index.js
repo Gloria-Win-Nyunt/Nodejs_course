04968eeceaa37980b1e9c4f294c0c709274d00d0
gloriabee <figmabebe@gmail.com>
2024-02-04 23:29:01 +0700

    global object

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+// globalThis.setTimeout(function () {
+//   console.log("hello");
+// },3000);
+
+let interval = globalThis.setInterval(function () {
+  console.log("hello");
+}, 2000);
+
+globalThis.setTimeout(function () {
+  clearInterval(interval);
+}, 5000);
+
+console.log(__dirname);
+console.log(__filename);
