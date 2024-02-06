7c81e56cadefbb83ec132fd34b2e785d42ec5259
gloriabee <figmabebe@gmail.com>
2024-02-06 12:51:02 +0700

    basic routing

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const http = require("http");
 const fs = require("fs");
 const server = http.createServer((req, res) => {
+  let fileName;
+  switch (req.url) {
+    case "/": //home.html
+      fileName = "index.html";
+      break;
+
+    case "/contact":
+      fileName = "contact.html";
+      break;
+
+    case "/about":
+      fileName = "about.html";
+      break;
+
+    default:
+      fileName = "404.html";
+      break;
+  }
   res.setHeader("Content-Type", "text/html");
 
   // res.write("<h1>Hello World</h1>");
   // res.write("<p>Hi There</p>");
 
-  fs.readFile("./view/index.html", (err, data) => {
+  fs.readFile("./view/" + fileName, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
