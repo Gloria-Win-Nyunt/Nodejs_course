68549d19bb1f52489a4600e95e12ed6973266497
gloriabee <figmabebe@gmail.com>
2024-02-07 11:06:14 +0700

    express redirection

--- app.js
+++ app.js
@@ -9,9 +9,20 @@ app.get("/about", (req, res) => {
   res.sendFile("./view/about.html", { root: __dirname });
 });
 
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
 app.get("/contact", (req, res) => {
   res.sendFile("./view/contact.html", { root: __dirname });
 });
+
+app.use((req, res) => {
+  res.status(404);
+  res.sendFile("./view/404.html", { root: __dirname });
+});
+
+
 app.listen(4000, () => {
   console.log("app is running on port 4000");
 });
